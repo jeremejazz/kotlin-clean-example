2d15f614fd7a0e906e73c44d1b485590f80cb2fb
Jereme Causing <jeremejazz15@gmail.com>
2025-12-24 12:48:22 +0800

    add database connection

---
+++ src/main/kotlin/data/db/DatabaseFactory.kt
@@ -0,0 +1,31 @@
+package me.jereme.data.db
+
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.Statement
+
+class DatabaseFactory {
+
+    private val URL = "jdbc:sqlite:phonebook.db"
+
+    fun connect(): Connection{
+        return DriverManager.getConnection(URL)
+    }
+
+    fun init(){
+        connect().use{
+            conn ->
+            val statement: Statement = conn.createStatement()
+            val sql = """
+                CREATE TABLE IF NOT EXISTS contacts(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT
+                    name TEXT NOT NULL,
+                    phone_number TEXT NOT NULL,
+                    email TEXT
+                );
+            """.trimIndent()
+            statement.execute(sql)
+        }
+    }
+}
+
